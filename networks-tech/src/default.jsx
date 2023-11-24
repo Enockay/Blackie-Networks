02599efa91962663f18fd412cf7c15a619c23b39
enockay <enockaymwema@gmail.com>
2023-11-24 22:08:33 +0300

    1 file added

--- networks-tech/src/default.jsx
+++ networks-tech/src/default.jsx
@@ -8,9 +8,9 @@ const InitialOffer = () => {
   };
 
   return (
-    <div>
+    <div className={`inital-offer ${userInteracted ? 'hidden' : 'animate'}`} >
       {!userInteracted && (
-        <div className='inital-offer'>
+        <div className="offer-content" >
           <h2>Special Offer!!</h2>
           <p className="item-1">1- DAY  package ksh 25 <button className="k-25"> Buy</button> </p>
           <p> WELCOME BONUS .Limited time upto date.</p>
