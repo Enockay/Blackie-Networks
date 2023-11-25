db69a436a74b3ce0b356738a60ba107db51b076d
enockay <enockaymwema@gmail.com>
2023-11-25 10:33:58 +0300

    1file added

--- networks-tech/src/packages.jsx
+++ networks-tech/src/packages.jsx
@@ -12,32 +12,32 @@ const [selectedButton, setSelectedButton] = useState(null);
     if (button === 'btn1') {
       content = (
         <div className='packages'>
-          <p className="item-1">30-min package ksh 10 <button className="k-10"> Buy</button> </p>
-          <p className="item-1">1-hour package ksh 20 <button className="k-20"> Buy</button> </p>
-          <p className="item-1">2-hour package ksh 30 <button className="k-30"> Buy</button> </p>
-          <p className="item-1">3-hour package ksh 50 <button className="k-50"> Buy</button> </p>
-          <p className="item-1">6-hour package ksh 100 <button className="k-100">Buy</button> </p>
+          <span className="item-1"><p className='p'>30-min package ksh=10</p> <button className="k-10"> Buy</button> </span>
+          <span className="item-1"><p className='p' >1-hour package ksh=20</p> <button className="k-20"> Buy</button> </span>
+          <span className="item-1"><p className='p'>2-hour package ksh=30</p> <button className="k-30"> Buy</button> </span>
+          <span className="item-1"><p className='p'>3-hour package ksh=50 </p><button className="k-50"> Buy</button> </span>
+          <span className="item-1"><p className='p'>6-hour package ksh=100 </p><button className="k-100">Buy</button> </span>
         </div>
       );
     } else if (button === 'btn2') {
       content = (
 
         <div className='packages'>
-          <p className="item-1">2 days package ksh 200 <button className="k-10"> Buy</button> </p>
-          <p className="item-1">3 days package ksh 250 <button className="k-20"> Buy</button> </p>
-          <p className="item-1">7 days package ksh 350 <button className="k-30"> Buy</button> </p>
-          <p className="item-1">10 days package ksh 450 <button className="k-50"> Buy</button> </p>
-          <p className="item-1">14 days package ksh 700 <button className="k-100">Buy</button> </p>
+          <span className="item-1"><p className='p'>2 days package ksh=200</p> <button className="k-10"> Buy</button> </span>
+          <span className="item-1"><p className='p'>3 days package ksh=250 </p><button className="k-20"> Buy</button> </span>
+          <span className="item-1"><p className='p'>7 days package ksh=350</p> <button className="k-30"> Buy</button> </span>
+          <span className="item-1"><p className='p'>10 days package ksh=450 </p><button className="k-50"> Buy</button> </span>
+          <span className="item-1"><p className='p'>14 days package ksh=700 </p><button className="k-100">Buy</button> </span>
         </div>
       );
     } else if (button === 'btn3') {
       content = (
         <div className='packages'>
-          <p className="item-1">20 days package ksh 1200 <button className="k-10"> Buy</button> </p>
-          <p className="item-1">30 days package ksh 1500 <button className="k-20"> Buy</button> </p>
-          <p className="item-1">10 days package ksh  300 <button className="k-30"> Buy</button> </p>
-          <p className="item-1">21 days package ksh 800 <button className="k-50"> Buy</button> </p>
-          <p className="item-1">28 days package ksh 1000 <button className="k-100">Buy</button> </p>
+          <span className="item-1"><p className='p'>20 days package ksh=1200</p> <button className="k-10"> Buy</button> </span>
+          <span className="item-1"><p className='p'>30 days package ksh=1500 </p><button className="k-20"> Buy</button> </span>
+          <span className="item-1"><p className='p'>10 days package ksh=300 </p><button className="k-30"> Buy</button> </span>
+          <span className="item-1"><p className='p'>21 days package ksh=800 </p><button className="k-50"> Buy</button> </span>
+          <span className="item-1"><p className='p'>28 days package ksh=1000</p> <button className="k-100">Buy</button> </span>
         </div>
       );
     } else if (button === 'btn4') {
