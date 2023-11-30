0ebb56aaab8989fd12cf6bb916292e0fcc23321d
enockay <enockaymwema@gmail.com>
2023-11-30 12:27:57 +0300

    1file added

--- networks-tech/src/userId/userid.jsx
+++ networks-tech/src/userId/userid.jsx
@@ -1,33 +1,42 @@
-
+import React, { useState } from 'react';
 
 const User = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [warning, setWarning] = useState('');
 
-    
-    const userNumber = 
-    (
-        <input type="num"  placeholder="mpesa-phone" className="number" required minLength={12}></input>
-    )
+  const handleInputChange = (event) => {
+    setPhoneNumber(event.target.value);
+    setWarning(''); // Clear the warning when the user starts typing
+  };
 
-
-    const checkBox = () =>{
-    if(userNumber === '')
-    {
-      <h3>phone Number for transcation</h3>
+  const checkBox = () => {
+    if (phoneNumber.length < 12) {
+      setWarning('Please enter a valid phone number with at least 12 digits.');
+    } else {
+      // Perform the logic for a valid phone number
+      setWarning(''); // Clear the warning if it was previously displayed
     }
-    else{
-        <h4>Number captured wait for STK in it</h4>
-    }
-}
+  };
 
+  return (
+    <>
+      <div className="input">
+        <input
+          type="number"
+          placeholder="mpesa-phone"
+          className="number"
+          required
+          minLength={12}
+          value={phoneNumber}
+          onChange={handleInputChange}
+        />
+        <button className="button" onClick={() => checkBox()}>
+          PAY
+        </button>
+      </div>
+      {warning && <p className="warning">{warning}</p>}
+    </>
+  );
+};
 
-    return  (
-        <>
-        <div class="input">
-         {userNumber}
-        <button className="button" onClick={()=>checkBox}>PAY</button>
-        </div>
-        
-        </>
-    )
-}
-export default User
+export default User;
