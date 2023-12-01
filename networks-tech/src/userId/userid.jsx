61d06d5438b7920f6c6b236663e128d439634085
enockay <enockaymwema@gmail.com>
2023-12-01 13:00:53 +0300

    1 file added

--- networks-tech/src/userId/userid.jsx
+++ networks-tech/src/userId/userid.jsx
@@ -6,15 +6,29 @@ const User = () => {
 
   const handleInputChange = (event) => {
     setPhoneNumber(event.target.value);
-    setWarning('start 2457'); // Clear the warning when the user starts typing
+    setWarning('start 2547XXXXXXXX'); 
   };
 
   const checkBox = () => {
     if (phoneNumber.length < 12) {
       setWarning('Please enter a valid phone number with at least 12 digits.');
     } else {
-      // Perform the logic for a valid phone number
-      setWarning(''); // Clear the warning if it was previously displayed
+       fetch("",{
+        method: 'post',
+        header :{
+            'Content-type' : 'application\json',
+        },
+        body :JSON.stringify({phoneNumber}),
+       })
+       .then((response) => response.json())
+       .then((data)=>{
+        console.log('server response:',data);
+       })
+       .catch((error)=>{
+        console.log(`server responded with ${error}`)
+       })
+
+      setWarning(''); 
     }
   };
 
