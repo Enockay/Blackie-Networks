8fba808eae174ad5c1ed1b40cce8d780c4eebe2b
enockay <enockaymwema@gmail.com>
2023-12-31 15:57:09 +0300

    modification

--- networks-tech/src/clientApis/statements.jsx
+++ networks-tech/src/clientApis/statements.jsx
@@ -3,9 +3,11 @@ import StatementTable from './renderStatement.jsx';
 
 const StatementRequest = ({ onRequestComplete }) => {
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [orderId, setOrderId] = useState('');
   const [isLoading, setLoading] = useState(false);
   const [receivedData,setData]  = useState(true);
   const [warn, setWarning] = useState('');
+  const [orderWarn, setOrderWarn] = useState('');
   
 
   const handleStatementRequest = async () => {
@@ -25,15 +27,25 @@ const StatementRequest = ({ onRequestComplete }) => {
      setData(data);
       onRequestComplete(data);
     } catch (error) {
-      console.error('Error requesting statements:', error);
+      setOrderWarn('Error requesting statements:', error);
     } finally {
       setLoading(false);
     }
   };
+
+  const handleInputChange = (event) => {
+    setPhoneNumber(event.target.value);
+    setWarning('Start 2547XXXXXXXX');
+  };
+
   const checkBox = () => {
-    if (phoneNumber.length !== 12) {
+    if (phoneNumber.length !== 12 ) {
       setWarning('Please enter a valid phone number with 12 digits.');
-    } else {
+    }else if (orderId.length < 4){
+      setWarning('');
+      setOrderWarn('This field should be filled or not correctly filled');
+    }
+     else {
       handleStatementRequest();
       setWarning('wait for the statements')
       setTimeout(() => {
@@ -44,27 +56,40 @@ const StatementRequest = ({ onRequestComplete }) => {
   };
 
   return (
-    <div>
-      <label>
+    <div className='statement-form'>
+      <label className='phone'>
         Phone Number:
         <input
-          type="text"
+          type="Number"
           value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
+          onChange={handleInputChange}
           className='input1'
         />
       </label>
-      {warn && <p style={{ color: warn === 'Start 2547XXXXXXXX' ? 'white' : 'red',
+      {warn && <p style={{ color: warn === 'Start 2547XXXXXXXX' ? 'white' : 'green',
        fontSize :'11px',marginTop : '2px' }}>
         {warn}
       </p>
       }
 
-      <button onClick={checkBox} disabled={isLoading}>
+      <label className='passcode'>
+         Your Passcode:
+         <input 
+         type = "text"
+         value = {orderId}
+         className='input1'
+         onChange={(e) =>setOrderId(e.target.value)}
+         ></input>
+      </label>
+
+       {orderWarn && <p style={{color: orderWarn === 'fill the passcode field' ? 'white' : 'green',
+       fontSize : '11px',marginTop : '2px'}}>
+        {orderWarn}
+        </p>}
+      <button onClick={checkBox} disabled={isLoading} className='getStatementB'>
         {isLoading ? 'Processing...' : 'Get Statements'}
       </button>
-       
-        
+      <p>NB : passcode is the transaction code that mpesa will sent you after successful payment of any package here eg RLU4NU9S68</p>
     </div>
   );
 };
