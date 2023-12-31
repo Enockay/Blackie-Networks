f87d3413a7a68a97c82e10e8ba32904d016f7baf
enockay <enockaymwema@gmail.com>
2023-12-31 23:06:31 +0300

    fixed

--- networks-tech/src/clientApis/statements.jsx
+++ networks-tech/src/clientApis/statements.jsx
@@ -5,10 +5,9 @@ const StatementRequest = ({ onRequestComplete }) => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [Order_Id, setOrderId] = useState('');
   const [isLoading, setLoading] = useState(false);
-  const [receivedData,setData]  = useState(true);
+  const [receivedData, setData] = useState([]);
   const [warn, setWarning] = useState('');
   const [orderWarn, setOrderWarn] = useState('');
-  
 
   const handleStatementRequest = async () => {
     try {
@@ -17,15 +16,18 @@ const StatementRequest = ({ onRequestComplete }) => {
       const response = await fetch("https://blackie-networks-295df9ed8dbf.herokuapp.com/api/getStatements", {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
-      },
-         body: JSON.stringify({ phoneNumber,Order_Id}),
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ phoneNumber, Order_Id }),
       });
 
       const data = await response.json();
-      //console.log(data);
-       setData(data);
-       onRequestComplete(data);
+      setData(data);
+      onRequestComplete(data);
+
+      if (data.length < 1) {
+        setOrderWarn(data.statements);
+      }
     } catch (error) {
       setOrderWarn('Error requesting statements:', error);
     } finally {
@@ -43,59 +45,67 @@ const StatementRequest = ({ onRequestComplete }) => {
   }
 
   const checkBox = () => {
-    if (phoneNumber.length !== 12 ) {
+    if (phoneNumber.length !== 12) {
       setWarning('Please enter a valid phone number with 12 digits.');
-    }else if (Order_Id.length < 4){
+    } else if (Order_Id.length < 4) {
       setWarning('');
       setOrderWarn('This field should be filled or not correctly filled');
-    }
-     else {
+    } else {
       handleStatementRequest();
       setTimeout(() => {
-        setOrderWarn('wait for the statements')
+        setOrderWarn('Wait for the statements');
         setWarning('');
         setOrderWarn('');
-      },5000 );
-      
+      }, 5000);
     }
   };
-  //console.log(Order_Id);
 
   return (
     <div className='statement-form'>
       <label className='phone'>
         Phone Number:
         <input
-          type="Number"
+          type="number"
           value={phoneNumber}
           onChange={handleInputChange}
           className='input1'
         />
       </label>
-      {warn && <p style={{ color: warn === 'Start 2547XXXXXXXX' ? 'white' : 'green',
-       fontSize :'11px',marginTop : '2px' }}>
+      {warn && <p style={{
+        color: warn === 'Start 2547XXXXXXXX' ? 'white' : 'green',
+        fontSize: '11px', marginTop: '2px'
+      }}>
         {warn}
       </p>
       }
 
       <label className='passcode'>
-         Your Passcode:
-         <input 
-         type = "text"
-         value ={Order_Id}
-         className='input1'
-         onChange={handleOrderChange}
+        Your Passcode:
+        <input
+          type="text"
+          value={Order_Id}
+          className='input1'
+          onChange={handleOrderChange}
         />
       </label>
 
-       {orderWarn && <p style={{color: orderWarn === 'fill the passcode field' ? 'white' : 'green',
-       fontSize : '11px',marginTop : '2px'}}>
+      {orderWarn && <p style={{
+        color: orderWarn === 'fill the passcode field' ? 'white' : 'green',
+        fontSize: '11px', marginTop: '2px'
+      }}>
         {orderWarn}
-        </p>}
+      </p>}
       <button onClick={checkBox} disabled={isLoading} className='getStatementB'>
         {isLoading ? 'Processing...' : 'Get Statements'}
       </button>
-      <p>NB : passcode is the transaction code that mpesa will sent you after successful payment of any package here eg RLU4NU9S68</p>
+
+      {receivedData.length === 1 ? (
+        <p style={{ color: 'green', marginTop: '10px' }}>{receivedData[0]}</p>
+      ) : (
+        <StatementTable data={receivedData} />
+      )}
+
+      <p>NB: passcode is the transaction code that M-Pesa will send you after successful payment of any package here, e.g., RLU4NU9S68</p>
     </div>
   );
 };
