032472548d414196e7993539a19ad6f1cf07e5c2
enockay <enockaymwema@gmail.com>
2024-01-06 11:26:18 +0300

    fixed

--- networks-tech/src/clientApis/loginApi.jsx
+++ networks-tech/src/clientApis/loginApi.jsx
@@ -21,6 +21,7 @@ const loginApi = async () => {
           const incomming = await response.json();
 
           const feedBack = incomming.responseState;
+          console.log(incomming);
           
          responseStatement = await feedBack
 
